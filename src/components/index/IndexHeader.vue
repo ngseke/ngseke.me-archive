c71fd520d9180915c4ffccab975dacc26842e115
Huang Xingqiao <a92304a92304@gmail.com>
2020-09-08 21:38:44 +0800

    update index header

--- src/components/index/IndexHeader.vue
+++ src/components/index/IndexHeader.vue
@@ -5,21 +5,11 @@ header.jumbotron.jumbotron-fluid.flex-vertical
       .col-6.col-md-12.text-center.d-flex.flex-column.align-items-center
         router-link.about-btn(to='about')
           h1.logo Xingqiao's Portfolio
-        a.btn.btn-light.scroll-down-btn(href='#' @click.prevent='scrollToMain')
-          fa(icon='angle-down')
 </template>
 
 <script>
-import Jump from 'jump.js'
-
 export default {
   name: 'IndexHeader',
-  methods: {
-    scrollToMain () {
-      const offset = -$('#nav').outerHeight()
-      Jump('main', { offset })
-    },
-  },
 }
 </script>
 
@@ -31,28 +21,27 @@ header.jumbotron
   background-image: url('~@/assets/img/bg/index-cover.jpg')
   background-size: cover
   background-color: transparent
+
   margin: 0
   padding: 0
   
   .logo
+    padding: 1rem
     font-family: 'Pacifico', cursive
     font-size: 3rem
-    color: $black
+    color: transparent
     text-transform: none
+    background: linear-gradient(-45deg, $black, darken($ngsek, 40%))
+    -webkit-background-clip: text
     
   .about-btn
     display: inline-block
     transition: transform .5s
-    margin-bottom: 5rem
+    margin-bottom: 3rem
     &:hover
       transform: scale(1.05)
       text-decoration: none
       @media (min-width: 768px)
         &::after
           transform: none
-          
-.scroll-down-btn
-  border-radius: 100rem
-  animation: scroll-down-btn 1s infinite
-  animation-direction: alternate
 </style>
