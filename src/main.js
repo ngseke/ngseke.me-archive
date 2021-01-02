f886ec5030c03202c9deff748eda04025194e107
Huang Xingqiao <a92304a92304@gmail.com>
2021-01-02 11:58:44 +0800

    install '@vue/composition-api'

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import VueMeta from 'vue-meta'
 import VueLazyload from 'vue-lazyload'
+import VueCompositionAPI from '@vue/composition-api'
 
 import 'bootstrap/js/dist/collapse.js'
 import 'bootstrap/js/dist/tooltip.js'
@@ -11,6 +12,7 @@ import './fontawesome'
 
 Vue.use(VueMeta)
 Vue.use(VueLazyload)
+Vue.use(VueCompositionAPI)
 Vue.config.productionTip = false
 
 new Vue({
