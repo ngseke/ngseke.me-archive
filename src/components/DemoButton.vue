f46abc243429a88c2dce91e88e05bc826568adba
Huang Xingqiao <a92304a92304@gmail.com>
2021-06-27 19:25:18 +0800

    create component DemoButton

---
+++ src/components/DemoButton.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+a.btn.btn-sm.btn-primary(:href='href' target='_blank')
+  slot Demo
+</template>
+
+<script>
+export default {
+  props: {
+    href: {
+      type: String,
+      require: true
+    }
+  }
+}
+</script>
