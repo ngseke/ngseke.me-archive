d2faaa5f9a4d2919d61fa946c391bfe31c9a3786
Huang Xingqiao <a92304a92304@gmail.com>
2021-06-27 19:11:00 +0800

    create component GithubButton

---
+++ src/components/GithubButton.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+a.btn.btn-sm.btn-github(:href='href' target='_blank')
+  fa(:icon='[`fab`, `github-alt`]')
+  |  Github
+</template>
+
+<script>
+export default {
+  props: {
+    href: {
+      type: String,
+      require: true
+    }
+  }
+}
+</script>
+
+<style scoped lang="sass">
+.btn-github
+  background-color: #0e2635
+  color: white
+  &:hover
+    color: white
+    background-color: #0b1d28
+</style>
