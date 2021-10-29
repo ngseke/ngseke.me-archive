888ee2a09376532033ff98056e7c376da82dbc2a
Huang Xingqiao <a92304a92304@gmail.com>
2021-10-29 20:28:08 +0800

    Remove v-lazy

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -6,9 +6,9 @@ div
     //- MCIP
     IndexSection(id='mcip' :bg='require(`@/assets/img/bg/mcip.jpg`)' :isTile='isTile')
       template(slot='img')
-        img.img-fluid.cover(v-lazy='require(`@/assets/img/cover/mcip.png`)' alt='MCIP Cover')
+        img.img-fluid.cover(src='@/assets/img/cover/mcip.png' alt='MCIP Cover')
       template(slot='content')
-        img(v-lazy='require(`@/assets/img/logo/mcip.svg`)' alt='樂台計畫')
+        img(src='@/assets/img/logo/mcip.svg' alt='樂台計畫')
         .title.text-light MCIP Official Website
         .subtitle.text-light #[b 樂台計畫]官方網站
         DetailButton(name='mcip')
@@ -18,9 +18,9 @@ div
     //- MCIP CMS
     IndexSection(id='mcip-cms' :bg='require(`@/assets/img/bg/mcip-cms.jpg`)' :isTile='isTile')
       template(slot='img')
-        img.img-fluid.cover(v-lazy='require(`@/assets/img/cover/mcip-cms.png`)' alt='MCIP CMS Cover')
+        img.img-fluid.cover(src='@/assets/img/cover/mcip-cms.png' alt='MCIP CMS Cover')
       template(slot='content')
-        img(v-lazy='require(`@/assets/img/logo/mcip-cms.svg`)' alt='樂台計畫')
+        img(src='@/assets/img/logo/mcip-cms.svg' alt='樂台計畫')
         .title.text-light MCIP CMS
         .subtitle.text-light #[b 樂台計畫]後台管理系統
         DetailButton(name='mcip-cms')
@@ -28,9 +28,9 @@ div
     //- EM Optimization Lab
     IndexSection(id='emo' :bg='require(`@/assets/img/bg/emo.jpg`)' :isTile='isTile')
       template(slot='img')
-        img.img-fluid.cover(v-lazy='require(`@/assets/img/cover/emo.png`)' alt='EM Optimization Lab Cover')
+        img.img-fluid.cover(src='@/assets/img/cover/emo.png' alt='EM Optimization Lab Cover')
       template(slot='content')
-        img(v-lazy='require(`@/assets/img/logo/emo.svg`)' alt='EM Optimization Lab Logo')
+        img(src='@/assets/img/logo/emo.svg' alt='EM Optimization Lab Logo')
         .title.text-light EM Optimization Lab
         .subtitle.text-light #[b 電磁最佳化實驗室]網站
         DetailButton(name='emo')
